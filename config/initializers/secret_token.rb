3a98c88b5f7bcc63a24fad8a6f2ae035e8979739
Moises Narvaez <moises.narvaez@koombea.com>
2012-04-10 11:53:51 -0500

    ONLY Devise & twitter-bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuel::Application.config.secret_token = 'd977939cbd19de31cc00b0dfc212ed3d9be83ce753e748bc968bef0ccc5aad47d021ae0e696fbb111347e00947ed390ee43d06847cc680b45d7993188f389df3'
